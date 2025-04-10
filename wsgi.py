fde9cb0bad1e77bfa5a5e44d12c478dcb5ce04c5
athenavi <support@7trees.cn>
2025-04-10 13:48:26 +0800

    fix(core): 修正日志文件创建逻辑并添加异常处理

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,13 @@ def main():
     if not os.path.exists('./temp/app.log'):
         print('日志文件未创建！')
         log_path = "temp"
-        os.makedirs(log_path)
-        file = open(log_path + "/app.log", "w", encoding='utf-8')
-        file.write("------zyBlog_V2.0------")
-        file.close()
+        try:
+            os.makedirs(log_path)
+        except FileExistsError:
+            print(f'目录 {log_path} 已存在，跳过创建。')
+        finally:
+            with open(log_path + "/app.log", "w", encoding='utf-8') as file:
+                file.write("------zyBlog_V2.0------")
         print('success------日志文件已自动创建！！(提示:若创建失败，你可以自行创建 temp 目录并在其中创建 app.log 文件)')
         print('现在你可以重新启动程序！')
         return
